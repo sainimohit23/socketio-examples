19a7fed32ad10e5f383fc4742e9397ca7b0d08f0
sainimohit23 <sainimohit23@gmail.com>
2020-04-09 22:26:49 +0530

    minor changes

--- audio/audio.py
+++ audio/audio.py
@@ -8,7 +8,7 @@ import wave
 from flask import Blueprint, current_app, session, url_for, render_template
 from flask_socketio import emit
 from socketio_examples import socketio
-import pyaudio
+# import pyaudio
 import numpy as np
 
 bp = Blueprint('audio', __name__, static_folder='static',
